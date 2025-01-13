b9c72f70ec2a12778ea9131539933207830baa19
mutiarahns <mutiarahns@gmail.com>
2025-01-13 20:23:31 +0700

    chore: tidy up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import { TodoApp } from "./components/shared/todo-app";
 
-// const taskItem = [
-//   { id: 1, text: "Task 1", isCompleted: false },
-//   { id: 2, text: "Task 2", isCompleted: true },
-//   { id: 3, text: "Task 3", isCompleted: true },
-// ];
-
 export function App() {
   return (
     <div>
